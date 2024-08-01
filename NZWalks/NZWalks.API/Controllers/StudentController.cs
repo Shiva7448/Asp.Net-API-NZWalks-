83be8ce4c86323060f8319a47886b39053e4396b
Shivam Singh <70027068+Shiva7448@users.noreply.github.com>
2024-08-02 00:02:12 +0530

    Add First Code

---
+++ NZWalks/NZWalks.API/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:portnumber/api/student
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents() 
+        {
+            string[] studentNames = new string[] { "Ram", "Lakshman", "Bharat", "Satrughan" };
+
+            return Ok(studentNames);
+        }
+    }
+
+}
