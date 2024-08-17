2ad36bb392b8f43fe9a0393f2a9435051285fff0
Shivam Singh <70027068+Shiva7448@users.noreply.github.com>
2024-08-17 22:51:07 +0530

    Add files via upload
    Added CRUD operations in API controller.

---
+++ Models/DTO/AddRegionsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class AddRegionsDto
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionImageUrl { get; set; }
+    }
+}
---
+++ Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LengthInKm { get; set; }
+        public string? WalkImageURL { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+        //Nevigation Properties
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+    }
+}
